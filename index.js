bc0a6f787f84a7be329b2f1ec81b379582fa4890
nah1222 <nah1222@yahoo.com>
2021-03-11 17:13:29 -0500

    insert

--- index.js
+++ index.js
@@ -14,5 +14,36 @@ class BST {
   size() {
     return this.count
   }
+
+  insert(value) {
+    this.count++
+    let newNode = new Node(value)
+
+    const searchTree = node => {
+      //if value <node.value, go left
+      if (value < node.value){
+        //if no left child, append new node
+        if (!node.left){
+          node.left = newNode
+        }
+        // if there is a left child, go left again
+        else {
+          searchTree(node.left)
+        }
+      }
+      //if value > node.value, go right
+      else if (value > node.value){
+        //if no right node append new node
+        if (!node.right) {
+          node.right = newNode
+        }
+        //if right child, look right again
+        else {
+          searchTree(node.right)
+        }
+      }
+    }
+    searchTree(this.root)
+  }
   
 }
